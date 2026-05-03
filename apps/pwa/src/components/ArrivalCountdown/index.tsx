4d4725bcb601cdb7c61502220aa2671b63fa4bab
malaika22 <malaikaafridi22@gmail.com>
2026-05-04 00:42:15 +0500

    arrival countdown

---
+++ apps/pwa/src/components/ArrivalCountdown/index.tsx
@@ -0,0 +1,8 @@
+export const ArrivalCountdown = () => {
+  return (
+    <div className="bg-[#CEC6B840] mx-auto border border-[#CEC6B8] w-fit rounded-[20px] text-[9px] py-[6px] px-[14px]  text-[#5C3530] tracking-[1.26px] uppercase ">
+      <span className="rounded-full bg-[#5C3530] w-[5px] h-[5px] inline-block mr-2" />
+      ARRIVES IN 2 DAYS
+    </div>
+  );
+};
