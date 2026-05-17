79c65bac29a922cfa7ca1a31db851cc04065ce9a
malaika22 <malaikaafridi22@gmail.com>
2026-05-17 18:32:39 +0500

    added status page

---
+++ apps/pwa/src/app/status/page.tsx
@@ -0,0 +1,10 @@
+import { PageLayout } from '@/components/AppLayout/PageLayout';
+import { Status } from '@/components/Status';
+
+export default function StatusPage() {
+  return (
+    <PageLayout>
+      <Status />
+    </PageLayout>
+  );
+}
