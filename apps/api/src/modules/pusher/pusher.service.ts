3f376146dd71f2f715c63c6bccf5c3fdcc936fb1
malaika22 <malaikaafridi22@gmail.com>
2026-06-10 17:01:57 +0500

    create pusher module

---
+++ apps/api/src/modules/pusher/pusher.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Pusher from 'pusher';
+
+@Injectable()
+export class PusherService {
+  private pusher: Pusher;
+
+  constructor(private config: ConfigService) {
+    this.pusher = new Pusher({
+      appId: config.getOrThrow<string>('PUSHER_APP_ID'),
+      key: config.getOrThrow<string>('PUSHER_KEY'),
+      secret: config.getOrThrow<string>('PUSHER_SECRET'),
+      cluster: config.getOrThrow<string>('PUSHER_CLUSTER'),
+      useTLS: true,
+    });
+  }
+
+  async trigger(channel: string, event: string, data: any) {
+    return this.pusher.trigger(channel, event, data);
+  }
+
+  authenticateChannel(socketId: string, channel: string) {
+    return this.pusher.authorizeChannel(socketId, channel, {
+      user_id: socketId,
+    });
+  }
+}
