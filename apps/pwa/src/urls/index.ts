b187cc5e284290f76a43fde33ab75fdf35927993
malaika22 <malaikaafridi22@gmail.com>
2026-06-10 22:17:43 +0500

    updated pwa urls

--- apps/pwa/src/urls/index.ts
+++ apps/pwa/src/urls/index.ts
@@ -4,7 +4,7 @@ import { config } from '@/config';
 export const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export const API_URLS = {
-  manifestLinkOpen: `${baseUrl}${API.manifest.linkOpen}`,
+  manifestLinkOpen: `${baseUrl}${API.manifest.linkOpened}`,
   oauthToken: `https://${config.AUTH0_DOMAIN}/oauth/token`,
   signOut: '/api/auth/signout',
 };
